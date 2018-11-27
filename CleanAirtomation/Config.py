f399c0582d43b24fe21de008f5c17be5eb9d8b32
loen <andrzej.pozlutko@gmail.com>
2018-11-27 22:13:35 +0100

    add SwitchBotConnector

--- CleanAirtomation/Config.py
+++ CleanAirtomation/Config.py
@@ -6,4 +6,7 @@ class Config:
     def read_config(self):
         with open("config.yaml", 'r') as stream:
             data_loaded = yaml.load(stream)
-            return {'installationId': data_loaded['installationId'], 'apikey': data_loaded['apikey']}
+            return {'installationId': data_loaded['installationId'],
+                    'apikey': data_loaded['apikey'],
+                    'commandPath': data_loaded['commandPath'],
+                    'mac': data_loaded['mac']}
--- clean_airtomation.py
+++ clean_airtomation.py
@@ -1,8 +1,12 @@
 from CleanAirtomation.AirlyDao import AirlyDao
 from CleanAirtomation.Config import Config
+from CleanAirtomation.SwitchBotConnector import SwitchBotConnector
 
 config = Config()
 conf = config.read_config()
 airly_dao = AirlyDao(conf['apikey'], conf['installationId'])
 print(airly_dao.caqi())
+switchBotConnector = SwitchBotConnector(conf['commandPath'], conf['mac'])
+result = switchBotConnector.push_the_button()
+print(result)
 
