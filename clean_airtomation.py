6b0c6e979387dc99454c14f097c072958fd35bc8
loen <andrzej.pozlutko@gmail.com>
2018-12-04 21:50:10 +0100

    added interval and interval setup

--- clean_airtomation.py
+++ clean_airtomation.py
@@ -1,7 +1,12 @@
+import time
+
 from CleanAirtomation import CleanAirtomationService
 from CleanAirtomation.Config import Config
 
 config = Config()
+check_interval_min = config.read_config()['checkIntervalInMinutes']
 cleanAirtomationService = CleanAirtomationService.CleanAirtomationService(config)
-cleanAirtomationService.clean_polluted_air()
+while True:
+    cleanAirtomationService.clean_polluted_air()
+    time.sleep(check_interval_min * 60)
 
