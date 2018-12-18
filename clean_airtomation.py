5e642c91539065451a3a0c84e636dc1a29acc629
loen <andrzej.pozlutko@gmail.com>
2018-12-18 20:50:09 +0100

    quickfix

--- clean_airtomation.py
+++ clean_airtomation.py
@@ -8,7 +8,7 @@ from clean_airtomation.config import Config
 config = Config()
 conf = config.read_config()
 airly_dao = AirlyDao(conf['airlyUrl'], conf['apikey'], conf['installationId'])
-air_purifier = AirPurifier.AirPurifier(config)
+air_purifier = AirPurifier(config)
 cleanAirtomationService = CleanAirtomationService(conf['caqiTreshold'], conf['cleaningPause'], airly_dao, air_purifier)
 while True:
     cleanAirtomationService.clean_polluted_air()
